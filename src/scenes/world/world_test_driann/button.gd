3610b7b370f77e40fe6d2237b4d48b6a9020f296
Driannn <drianbetancourt@gmail.com>
2026-04-30 19:33:25 -0300

    botones para testear audio

--- src/scenes/world/world_test_driann/button.gd
+++ src/scenes/world/world_test_driann/button.gd
@@ -1,10 +1,10 @@
 extends Button
 
 @export var bip_sfx: AudioStreamPlayer2D
-
+@export var delete_on_pressed: bool
 
 func _on_pressed() -> void:
 
 	AudioManager.play_detached_2d_sfx(bip_sfx, global_position)
-
-	queue_free()
+	
+	if delete_on_pressed: queue_free()
