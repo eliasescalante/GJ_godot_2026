532b6bbf35ae0818be30a1635cd44ae29424998b
Driannn <drianbetancourt@gmail.com>
2026-05-01 12:18:46 -0300

    audio manager listo y testeado

--- src/scenes/world/world_test_driann/button.gd
+++ src/scenes/world/world_test_driann/button.gd
@@ -3,12 +3,13 @@ extends Button
 @export var bip_sfx: AudioStreamPlayer2D
 @export var delete_on_pressed: bool
 @export var play_detached: bool
+@export var sfx_start_offset: float = 1.5
 
 func _on_pressed() -> void:
 
 	if play_detached:
-		AudioManager.play_detached_2d_sfx(bip_sfx, global_position)
+		AudioManager.play_detached_2d_sfx(bip_sfx, global_position, sfx_start_offset)
 	else:
-		bip_sfx.play(2)
+		bip_sfx.play(sfx_start_offset)
 	
 	if delete_on_pressed: queue_free()
--- src/scripts/AudioManager.gd
+++ src/scripts/AudioManager.gd
@@ -15,7 +15,7 @@ func play_sfx(player: AudioStreamPlayer) -> void:
 	player.play()
 
 ## Reproduce un sfx en la posicion 2d correcta aunque su parent ya no este instanciado 
-func play_detached_2d_sfx(stream_player_2d: AudioStreamPlayer2D, position: Vector2) -> void:
+func play_detached_2d_sfx(stream_player_2d: AudioStreamPlayer2D, position: Vector2, start_time: float = 0.0) -> void:
 	if not stream_player_2d:
 		push_warning("Stream_player_2d not assigned.")
 		return
@@ -23,5 +23,6 @@ func play_detached_2d_sfx(stream_player_2d: AudioStreamPlayer2D, position: Vecto
 	var sfx_clone := stream_player_2d.duplicate() as AudioStreamPlayer2D
 	add_child(sfx_clone)
 	sfx_clone.global_position = position
-	sfx_clone.play()
+	
+	sfx_clone.play(start_time)
 	sfx_clone.finished.connect(sfx_clone.queue_free)
