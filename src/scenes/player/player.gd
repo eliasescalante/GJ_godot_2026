8880bcd2b4045f062e67e310e2038b7e5ca577c4
eliasescalante <eliasw.escalante@gmail.com>
2026-05-01 19:10:55 -0300

    agrego detector de item

--- src/scenes/player/player.gd
+++ src/scenes/player/player.gd
@@ -1,25 +1,41 @@
 extends CharacterBody2D
 
-
 @export var SPEED = 300.0
 @onready var personaje = $Cuerpo
+var pared_en_frente = false
+
+
+func _ready():
+	pass
+
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	
 	if direction != Vector2.ZERO:
 		velocity = direction * SPEED
-		#aca hice esto para dar vuelta el sprite cuando me muevo
-		var angulito_del_movimiento = direction.angle()
-		personaje.rotation = angulito_del_movimiento -(PI / 2)
+		var angulo = direction.angle()
+		personaje.rotation = angulo - (PI / 2)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	
 	move_and_slide()
 
-
+#logica villera para detectar cuerpecitos diferentes:
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	#para detectar items en modo test por el momento
-	#seguro usamos un group del tipo items por ahora cuadradito
-	if body.is_in_group("cuadradito"):
-		print("te vi")
+	if body.is_in_group("pared"):
+		print("esta la pared")
+		pared_en_frente = true
+
+
+func _on_detector_item_body_entered(body: Node2D) -> void:
+	if body.is_in_group("cuadradito") and not pared_en_frente:
+		print("veo el item")
+	elif body.is_in_group("cuadradito") and pared_en_frente:
+		print("veo el item pero esta tapado por la pared")
+
+
+func _on_detector_pared_body_exited(body: Node2D) -> void:
+	if body.is_in_group("pared"):
+		pared_en_frente = false
+		print("ya no veo la pared")
