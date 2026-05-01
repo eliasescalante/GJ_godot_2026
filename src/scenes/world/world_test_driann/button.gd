5f0a7e4454159ec87aa8c1f7a58e50197fe7695a
Driannn <drianbetancourt@gmail.com>
2026-05-01 09:55:22 -0300

    Test play detached sfx en drian_test. Listener como child de la camara
    Test play detached sfx en drian_test. exported variable para testear detached o no, y deleted on pressed o no
    
    Listener como child de la camara para el paneo de sonidos

--- src/scenes/world/world_test_driann/button.gd
+++ src/scenes/world/world_test_driann/button.gd
@@ -2,9 +2,13 @@ extends Button
 
 @export var bip_sfx: AudioStreamPlayer2D
 @export var delete_on_pressed: bool
+@export var play_detached: bool
 
 func _on_pressed() -> void:
 
-	AudioManager.play_detached_2d_sfx(bip_sfx, global_position)
+	if play_detached:
+		AudioManager.play_detached_2d_sfx(bip_sfx, global_position)
+	else:
+		bip_sfx.play()
 	
 	if delete_on_pressed: queue_free()
