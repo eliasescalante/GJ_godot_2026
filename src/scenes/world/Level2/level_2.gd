df795f9c76f2c1beacd291a994a3b8f01339aa24
eliasescalante <eliasw.escalante@gmail.com>
2026-05-02 15:21:14 -0300

    area en level2 que es el 1

--- src/scenes/world/Level2/level_2.gd
+++ src/scenes/world/Level2/level_2.gd
@@ -6,3 +6,8 @@ func _ready() -> void:
 	AudioManager.ambiance_day.play()
 	if AudioManager.ambiance_night.playing:
 		AudioManager.ambiance_night.stop()
+
+
+func _on_me_duermo_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://src/scenes/world/Level1/level_1.tscn")
